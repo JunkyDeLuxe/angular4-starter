eaee253244e6eb9f8d16afbac59b1d8dd45d9b89
Simon Costa <costapasquier.simon@gmail.com>
2017-02-12 22:02:38 +0100

    routings: Add routes for app

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+
+/** modules **/
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+];
+
+export const routing = RouterModule.forRoot(routes);
