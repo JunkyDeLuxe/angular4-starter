57ade755872851b38b254eeaae9753e9078d3bf9
Simon Costa <costapasquier.simon@gmail.com>
2017-02-12 22:02:21 +0100

    about: Add about page component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'my-about',
+    template: require('./about.component.html')
+})
+
+export class AboutComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {
+        console.log('Hello About');
+    }
+}
