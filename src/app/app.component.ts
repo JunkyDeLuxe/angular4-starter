35699b4f8dff77dc5941f9356278672d7787196e
Simon Costa <costapasquier.simon@gmail.com>
2017-02-12 18:09:45 +0100

    src: Add src first files like app, vendor and polyfills

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: require('./app.component.html')
+    //templateUrl: './app.component.html'
+})
+
+export class AppComponent { }
