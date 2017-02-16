877c5dbb14b2dc376a41dd1723aa03cbf357452e
JunkyDeLuxe <costapasquier.simon@gmail.com>
2017-02-16 13:53:47 +0100

    package: Add default language with dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpModule, Http } from '@angular/http';
 import { BrowserModule }  from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // ui-bootstrap //
+import { DropdownModule } from 'ng2-bootstrap'; // ui-bootstrap //
 import { TranslateStaticLoader, TranslateLoader, TranslateModule } from 'ng2-translate';
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import './app.module.less';
     imports: [
         BrowserModule,
         HttpModule,
-        NgbModule.forRoot(),
+        DropdownModule.forRoot(),
         TranslateModule.forRoot({
             provide: TranslateLoader,
             useFactory: (http: Http) => new TranslateStaticLoader(http, '/public/locales', '.json'),
