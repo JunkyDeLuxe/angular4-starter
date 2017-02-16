ac232f7b8e4b5f08fc1894fc084e320efdd2e0a6
JunkyDeLuxe <costapasquier.simon@gmail.com>
2017-02-16 18:11:01 +0100

    about: Modifiy about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-const _styles_ = require('!raw-loader!less-loader!./about.component.less');
-
 @Component({
     selector: 'my-about',
-    template: require('./about.component.html'),
-    styles: [ _styles_ ]
+    template: require('./about.component.html')
 })
 
 export class AboutComponent implements OnInit {
