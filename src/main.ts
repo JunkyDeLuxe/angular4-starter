1cf763a07112febf293d49036231c513fc42a351
Simon Costa <simon.costa@neo9.fr>
2017-10-03 12:58:26 +0200

    main: Add enable prod mod from angular

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 export function main() {
     platformBrowserDynamic().bootstrapModule(AppModule);
+}
+
+const ENV: any = process.env.ENV;
+
+if (ENV && ENV.mode !== 'development') {
+    enableProdMode();
 }
 
 if (document.readyState === 'complete') {
